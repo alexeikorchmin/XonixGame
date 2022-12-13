2b0a91c946767c239b9d3b60d9915d79927bc145
alexeikorchmin <alexeikorchmin@yandex.ru>
2022-12-13 13:06:38 +0300

    add ui

--- Assets/Scripts/Movement/PlayerMover.cs
+++ Assets/Scripts/Movement/PlayerMover.cs
@@ -3,24 +3,33 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour
 {
     [SerializeField] private GameField gameField;
+    [SerializeField] private bool isMoveActive;
+
+    public void SetPlayerMoveState(bool isActive)
+    {
+        isMoveActive = isActive;
+    }
 
     private void Update()
     {
+        if (!isMoveActive)
+            return;
+
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             gameField.MovePlayer(new Position(0, -1));
         }
-        
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             gameField.MovePlayer(new Position(0, 1));
         }
-        
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             gameField.MovePlayer(new Position(-1, 0));
         }
-        
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             gameField.MovePlayer(new Position(1, 0));
---
+++ Assets/Scripts/UI/ManagerUI.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ManagerUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text lifeText;
+    [SerializeField] private TMP_Text fieldPercentsText;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject winPanel;
+    [SerializeField] private Button playAgainButton;
+    [SerializeField] private Button nextLevelButton;
+
+    public void UpdateLifeText(int lives)
+    {
+        lifeText.text = "Lives: " + lives;
+    }
+
+    public void UpdatePercentText(float percent)
+    {
+        fieldPercentsText.text = Math.Round(percent, 2) + "%";
+    }
+
+    public void ShowGameOverPanel()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void ShowWinPanel()
+    {
+        winPanel.SetActive(true);
+    }
+
+    private void Awake()
+    {
+        playAgainButton.onClick.AddListener(delegate { StartGame(3); });
+        nextLevelButton.onClick.AddListener(delegate { StartGame(0); });
+    }
+
+    private void StartGame(int lives)
+    {
+        gameOverPanel.SetActive(false);
+        winPanel.SetActive(false);
+    }
+}
